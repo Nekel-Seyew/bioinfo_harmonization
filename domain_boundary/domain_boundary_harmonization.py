a3a36d57ad34d4d4f25de8cc8b630a4fbcabb02d
Mara Staines <mstaines@nd.edu>
2017-10-31 21:28:06 -0400

    Started reimplementation of Angov paper

---
+++ domain_boundary/domain_boundary_harmonization.py
@@ -0,0 +1,26 @@
+import sys
+
+proteins = {'TCA': 'S', 'AAT': 'N', 'TGG': 'W', 'GAT': 'D', 'GAA': 'E', 'TTC': 'F', 'CCG': 'P',
+           'ACT': 'T', 'GGG': 'G', 'ACG': 'T', 'AGA': 'R', 'TTG': 'L', 'GTC': 'V', 'GCA': 'A',
+           'TGA': '*', 'CGT': 'R', 'CAC': 'H', 'CTC': 'L', 'CGA': 'R', 'GCT': 'A', 'ATC': 'I',
+           'ATA': 'I', 'TTT': 'F', 'TAA': '*', 'GTG': 'V', 'GCC': 'A', 'GAG': 'E', 'CAT': 'H',
+           'AAG': 'K', 'AAA': 'K', 'GCG': 'A', 'TCC': 'S', 'GGC': 'G', 'TCT': 'S', 'CCT': 'P',
+           'GTA': 'V', 'AGG': 'R', 'CCA': 'P', 'TAT': 'Y', 'ACC': 'T', 'TCG': 'S', 'ATG': 'M',
+           'TTA': 'L', 'TGC': 'C', 'GTT': 'V', 'CTT': 'L', 'CAG': 'Q', 'CCC': 'P', 'ATT': 'I',
+           'ACA': 'T', 'AAC': 'N', 'GGT': 'G', 'AGC': 'S', 'CGG': 'R', 'TAG': '*', 'CGC': 'R',
+           'AGT': 'S', 'CTA': 'L', 'CAA': 'Q', 'CTG': 'L', 'GGA': 'G', 'TGT': 'C', 'TAC': 'Y',
+           'GAC': 'D'}
+host_freq = sys.argv[1]
+orig_freq = sys.argv[2]
+
+host_dict = {}
+orig_dict = {}
+with open(host_freq) as f:
+	for line in f:
+		line = line.rstrip().rsplit()
+		host_dict[line[0]] = float(line[1])
+
+with open(orig_freq) as f:
+	for line in f:
+		line = line.rstrip().rsplit()
+		orig_dict[line[0]] = float(line[1])
